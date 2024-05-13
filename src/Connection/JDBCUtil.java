36760e2b26b79da1f04f80f2957b8d82d01c7053
Hữu Nghĩa <buihuunghia31082004@gmail.com>
2024-05-14 01:16:56 +0700

    thêm giao diện quản lý khách hàng

--- src/Connection/JDBCUtil.java
+++ src/Connection/JDBCUtil.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Connection;
 
 import java.sql.Connection;
@@ -9,10 +6,6 @@ import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
-/**
- *
- * @author buihu
- */
 public class JDBCUtil {
 
     private static DatabaseMetaData mtdt;
--- src/Connection/testJDBCUtil.java
+++ src/Connection/testJDBCUtil.java
@@ -1,33 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author buihu
- */
 public class testJDBCUtil {
+
     public static void main(String[] args) throws SQLException,
             ClassNotFoundException {
         Connection con = JDBCUtil.getConnection();
-        
+
         JDBCUtil.printInfo(con);
-        
-        
+
         JDBCUtil.closeConnection(con);
-        
 
     }
-     public static Connection login(String userName, String password) throws SQLException,
+
+    public static Connection login(String userName, String password) throws SQLException,
             ClassNotFoundException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
-       String url = "jdbc:oracle:thin:@localhost:1521/orcl";
+        String url = "jdbc:oracle:thin:@localhost:1521/orcl";
         Connection con = DriverManager.getConnection(url, userName, password);
         return con;
     }
